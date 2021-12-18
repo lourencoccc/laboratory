582f413ca2647887233b88ac2c15acddb58be0e4
Joao L. Souza Jr <lourencoccc@gmail.com>
2021-12-18 19:03:47 -0300

    adicionado playlist Heitor

---
+++ playlist/download.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+# youtube-dl -x --audio-quality 0 --audio-format mp3 
+
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=ei2-RjJDBHc
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=90w2RegGf9w
+
+#Leo
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=mSO6PGFIETk
+
+#Transform superthero
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=tP9s_Lf_AQk
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=oPN2uwxs4Z4
+
+#Skank
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=7Ie4oL17Nwc
+
+#Sandy Junior
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=hpwHfbE8J2U
+
+#Bob Marley
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=HNBCVM4KbUM
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=69RdQFDuYPI
+
+#Jurassic
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=FkVfNf8ADec
+
+#Iron Meidan
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=J0N1yY937qg
+
+#galinha pintadinha
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=l7VsurR48Ew
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=ei2-RjJDBHc
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=EnzS35xpqao
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=h9DDiQLAVW0
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=MuBgIfBR1kA
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=b1dYkXjj-1o
+
+#bobz
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=MgG13r2fVOw
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=-fVH_PooFJI
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=vDee2bF8Xls
+
+
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=AD25rGt5x00
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=5TVsXxsFJps
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=BFCePyavZbg
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=siVfbM-jicg
+
+
+# Patati Patata
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=kUH15yq6Eto
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=fLFacU8tKT0
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=jvX9eozwbtU
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=CuunfAjpARY
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=jvX9eozwbtU
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=u6Y706ag0is
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=tvDZbnDNolM
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=PT8Tf02U3XQ
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=Dvkrh6L6E7E
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=rb7Lh4nxZ_w
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=rk_PovR6XYw
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=tFaME3V6Ueg
+
+# Palavra cantada
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=LK4JU7JX7eM
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=KYS47ZFZyBg
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=v1Pv9cOy-2s
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=WkC8BMxIWFk
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=V7i8ntY1zd8
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=6Q5p5vrIsUc
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=gsSY0i_-Ypw
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=lIGBBWrSOd8
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=00YqFumd344
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=DyEq-BL32tY
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=JHOaHqNGKbg
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=XWUNiI_1pWg
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=GI-WiE8b68Q
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=-fVH_PooFJI
+
+
+#English
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=XqZsoesa55w
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=Py4fhsx3Ct4
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=6R0v0ovq1hs
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=90w2RegGf9w
+
+#Bea
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=HzvDofigTKQ
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=m2uTFF_3MaA
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=YqB8Dm65X18
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=_7xMfIp-irg
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=YkgkThdzX-8
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=2Q_ZzBGPdqE
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=NCtzkaL2t_Y
+
+
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=sTelrncNFbU
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=q_3pZFaQnS8
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=uPFFw5hgr1U
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=ZkGDCoT5Ma4
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=w549t85naFE
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=Y0Zv345KY7s
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=TxtvogVepSg
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=JC33Ak17ZAo
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=o-af2fBXr3U
+youtube-dl -x --audio-quality 0 --audio-format mp3 https://www.youtube.com/watch?v=tF-FuxnNDvA
---
+++ playlist/readme.md
@@ -0,0 +1,87 @@
+
+
+sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+
+
+
+https://www.youtube.com/watch?v=ei2-RjJDBHc
+https://www.youtube.com/watch?v=90w2RegGf9w
+
+Leo
+https://www.youtube.com/watch?v=mSO6PGFIETk
+
+Transform superthero
+https://www.youtube.com/watch?v=tP9s_Lf_AQk
+
+Sandy Junior
+https://www.youtube.com/watch?v=hpwHfbE8J2U
+
+Skank
+https://www.youtube.com/watch?v=7Ie4oL17Nwc
+
+Bob Marley
+https://www.youtube.com/watch?v=HNBCVM4KbUM
+https://www.youtube.com/watch?v=69RdQFDuYPI
+
+Jurassic
+https://www.youtube.com/watch?v=FkVfNf8ADec
+
+Iron Meidan
+https://www.youtube.com/watch?v=J0N1yY937qg
+
+galinha pintadinha
+https://www.youtube.com/watch?v=ei2-RjJDBHc
+https://www.youtube.com/watch?v=EnzS35xpqao
+https://www.youtube.com/watch?v=h9DDiQLAVW0
+https://www.youtube.com/watch?v=l7VsurR48Ew
+https://www.youtube.com/watch?v=MuBgIfBR1kA
+https://www.youtube.com/watch?v=b1dYkXjj-1o
+
+bobz
+https://www.youtube.com/watch?v=MgG13r2fVOw
+https://www.youtube.com/watch?v=-fVH_PooFJI
+https://www.youtube.com/watch?v=vDee2bF8Xls
+
+Patati Patata
+https://www.youtube.com/watch?v=kUH15yq6Eto
+https://www.youtube.com/watch?v=fLFacU8tKT0
+https://www.youtube.com/watch?v=jvX9eozwbtU
+
+
+
+Palavra cantada
+https://www.youtube.com/watch?v=LK4JU7JX7eM
+https://www.youtube.com/watch?v=KYS47ZFZyBg
+https://www.youtube.com/watch?v=v1Pv9cOy-2s
+https://www.youtube.com/watch?v=WkC8BMxIWFk
+https://www.youtube.com/watch?v=V7i8ntY1zd8
+https://www.youtube.com/watch?v=6Q5p5vrIsUc
+https://www.youtube.com/watch?v=gsSY0i_-Ypw
+https://www.youtube.com/watch?v=lIGBBWrSOd8
+https://www.youtube.com/watch?v=00YqFumd344
+https://www.youtube.com/watch?v=DyEq-BL32tY
+https://www.youtube.com/watch?v=JHOaHqNGKbg
+https://www.youtube.com/watch?v=XWUNiI_1pWg
+https://www.youtube.com/watch?v=GI-WiE8b68Q
+https://www.youtube.com/watch?v=-fVH_PooFJI
+
+
+
+English
+https://www.youtube.com/watch?v=XqZsoesa55w
+https://www.youtube.com/watch?v=Py4fhsx3Ct4
+https://www.youtube.com/watch?v=6R0v0ovq1hs
+https://www.youtube.com/watch?v=90w2RegGf9w
+
+Bea
+https://www.youtube.com/watch?v=HzvDofigTKQ
+https://www.youtube.com/watch?v=m2uTFF_3MaA
+https://www.youtube.com/watch?v=YqB8Dm65X18
+https://www.youtube.com/watch?v=_7xMfIp-irg
+https://www.youtube.com/watch?v=YkgkThdzX-8
+https://www.youtube.com/watch?v=2Q_ZzBGPdqE
+https://www.youtube.com/watch?v=NCtzkaL2t_Y
+
+
+Senorita
+Despacito
